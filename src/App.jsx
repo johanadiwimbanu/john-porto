53bbcaa114f19b2c1e67414f5b901c21aeafb8d9
johanadiwimbanu <johanadiwimbanu@gmail.com>
2025-02-02 21:36:06 +0700

    fix: hidden crosshair in mobile device

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,14 @@ import { motion, AnimatePresence } from 'framer-motion';
 import EntranceTour from './components/EntranceTour';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const containerRef = useRef(null);
   const [isTourBegins, setIsTourBegins] = useState(false);
 
   const effectOptions = getRandomHyperspeedPresets();
   return (
     <div id='App' ref={containerRef}>
-      <Crosshair containerRef={containerRef} color='#ffffff' />
+      {isMobile && <Crosshair containerRef={containerRef} color='#ffffff' />}
       <AnimatePresence mode='wait'>
         <div className='z-10 w-full'>
           {isTourBegins ? (
